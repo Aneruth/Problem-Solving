b959007e39e06860b85baf6853f3299bde4d4a36
Aneruth Mohanasundaram <ane1998@gmail.com>
2021-07-30 16:37:16 +0200

    new changes

--- LeetCode/debug.py
+++ LeetCode/debug.py
@@ -256,10 +256,39 @@
 # print(dailyTemperatures(temperatures))
 
 
-def minDeletionSize(strs):
-    result = 0
-    for i in range(len(strs[0])):
-        temp = [x[i] for x in strs]
-        result += 0 if temp == sorted(temp) else 1
-    return result
-print(minDeletionSize(["cba","daf","ghi"]))
+# def minDeletionSize(strs):
+#     result = 0
+#     for i in range(len(strs[0])):
+#         temp = [x[i] for x in strs]
+#         result += 0 if temp == sorted(temp) else 1
+#     return result
+# print(minDeletionSize(["cba","daf","ghi"]))
+
+# def search1(alist,target):
+#     for i in alist:
+#         if i == target:
+#             return alist.index(target) 
+#     return -1
+# print(f'Method 2 output is: {search1([4,5,6,7,0,1,2],2)}')
+
+# Search in Rotated Sorted Array
+# You are given an integer array nums sorted in ascending order (with distinct values), and an integer target.
+# Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [[0,1,2,4,5,6,7]] might become [[4,5,6,7,0,1,2]]).
+# If target is found in the array return its index, otherwise, return -1.
+
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+ 
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+
+def search(alist,target):
+    if target in alist: return alist.index(target)
+    return -1
+print(f'Method 1 output is: {search([4,5,6,7,0,1,2],2)}')
